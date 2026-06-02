9a6147b94dcf8a6955655fdb67589bf7825fd3e0
北啊呢 <177828525@qq.com>
2026-06-03 02:14:51 +0800

    使用github.com/microcosm-cc/bluemonday库优化XXS过滤

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/gorilla/websocket v1.5.3
+	github.com/microcosm-cc/bluemonday v1.0.27
 	github.com/swaggo/files v1.0.0
 	github.com/swaggo/gin-swagger v1.3.0
 	github.com/swaggo/swag v1.16.6
@@ -20,6 +21,7 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
@@ -36,6 +38,7 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
--- backend/utils/md.go
+++ backend/utils/md.go
@@ -2,32 +2,76 @@ package utils
 
 import (
 	"bytes"
-	"regexp"
 
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/yuin/goldmark"
 )
 
-// 简单的XSS过滤
-var unsafePatterns = []*regexp.Regexp{
-	regexp.MustCompile(`<script[^>]*>.*?</script>`),
-	regexp.MustCompile(`<iframe[^>]*>.*?</iframe>`),
-	regexp.MustCompile(`<object[^>]*>.*?</object>`),
-	regexp.MustCompile(`<embed[^>]*>.*?</embed>`),
-	regexp.MustCompile(`on\w+\s*=\s*["']?[^"']*["']?`),
-}
+var forumPolicy = func() *bluemonday.Policy {
+
+	p := bluemonday.UGCPolicy()
+
+	// 图片
+	p.AllowElements("img")
+	p.AllowAttrs(
+		"src",
+		"alt",
+		"title",
+		"width",
+		"height",
+	).OnElements("img")
+
+	// 视频
+	p.AllowElements("video", "source")
+	p.AllowAttrs(
+		"src",
+		"type",
+		"controls",
+		"width",
+		"height",
+		"poster",
+	).OnElements("video", "source")
+
+	// 音频
+	p.AllowElements("audio")
+	p.AllowAttrs(
+		"src",
+		"type",
+		"controls",
+	).OnElements("audio")
+
+	// iframe（B站、YouTube）
+	p.AllowElements("iframe")
+	p.AllowAttrs(
+		"src",
+		"width",
+		"height",
+		"frameborder",
+		"allowfullscreen",
+		"allow",
+	).OnElements("iframe")
+
+	// 代码高亮
+	p.AllowAttrs("class").Globally()
+
+	// data-*
+	p.AllowDataAttributes()
+
+	// 允许协议
+	p.AllowURLSchemes(
+		"http",
+		"https",
+	)
+
+	return p
+}()
 
 func MarkdownToHTML(md string) string {
 	var buf bytes.Buffer
+
 	if err := goldmark.Convert([]byte(md), &buf); err != nil {
 		return md
 	}
 
-	html := buf.String()
-
-	// 简单过滤危险标签和事件
-	for _, pattern := range unsafePatterns {
-		html = pattern.ReplaceAllString(html, "")
-	}
-
-	return html
+	return forumPolicy.Sanitize(buf.String())
 }
